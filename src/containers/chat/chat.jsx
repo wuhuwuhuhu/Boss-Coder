564fbedce6dc7222027b2e9ae189e2bd8f432375
whd <wuhd1234@gmail.com>
2021-02-20 18:17:55 -0500

    Add: emojis to chat

--- src/containers/chat/chat.jsx
+++ src/containers/chat/chat.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
-import {NavBar, List, InputItem} from 'antd-mobile'
+import {NavBar, List, InputItem, Grid, Icon} from 'antd-mobile'
 
 
 import {sendMsg} from '../../redux/actions'
@@ -9,7 +9,34 @@ const Item = List.Item
 class Chat extends Component {
 
     state = {
-        content: ''
+        content: '',
+        isShowEmojis: false
+    }
+
+
+    constructor(props){
+        super(props)
+        const emojis = ["😀","😃","😄","😁","😆","😅","😂","🤣","🥲","☺️","😊","😇","🙂","🙃","😉","😌","😍","🥰","😘","😗","😙","😚","😋","😛","😝","😜","🤪","🤨","🧐","🤓","😎","🥸","🤩","🥳","😏","😒","😞","😔","😟","😕","🙁","☹️","😣","😖","😫","😩","🥺","😢","😭","😤","😠","😡","🤬","🤯","😳","🥵","🥶","😱","😨","😰","😥","😓","🤗","🤔","🤭","🤫","🤥","😶","😐","😑","😬","🙄","😯","😦","😧","😮","😲","🥱","😴","🤤","😪","😵","🤐","🥴","🤢","🤮","🤧","😷","🤒","🤕","🤑","🤠","😈","👿","👹","👺","🤡","💩","👻","💀","☠️","👽","👾","🤖","🎃","😺","😸","😹","😻","😼","😽","🙀","😿","😾"]
+        this.emojis = emojis.map(emoji => ({text: emoji}))
+    }
+
+    componentDidMount() {
+        //scroll to bottom
+        window.scrollTo(0, document.body.scrollHeight)
+    }
+    
+    componentDidUpdate() {
+        window.scrollTo(0, document.body.scrollHeight)
+    }
+    toggleShow = () => {
+        const isShowEmojis = !this.state.isShowEmojis
+        this.setState({isShowEmojis})
+        if(isShowEmojis){
+            setTimeout(() => {
+                //to solve emojis display bug
+                window.dispatchEvent(new Event('resize'))
+            })
+        }
     }
 
     handleSend = () => {
@@ -26,7 +53,8 @@ class Chat extends Component {
         }
         //clear input
         this.setState({
-            content: ''
+            content: '',
+            isShowEmojis: false
         })
     }
     render(){
@@ -49,8 +77,12 @@ class Chat extends Component {
 
         return(
             <div id='chat-page'>
-                <NavBar>{target.username}</NavBar>
-                <List>
+                <NavBar 
+                icon={<Icon type='left'></Icon>} 
+                onLeftClick={() => this.props.history.goBack()}
+                className='sticky-header'
+                >{target.username}</NavBar>
+                <List style={{marginTop: 50, marginBottom: 50,}}>
                     {
                         chatMsgs.map(msg => {
                             if(msg.from === targetId){
@@ -63,8 +95,26 @@ class Chat extends Component {
                     }
                 </List> 
 
-                <div className='am-tar-bar'>
-                    <InputItem placeholder='please input here' value={this.state.content}onChange={val => this.setState({content: val})} extra={<span onClick={this.handleSend}>send</span>}></InputItem>
+                <div className='am-tab-bar'>
+                    <InputItem placeholder='please input here' 
+                    style={{"letter-spacing":"1px"}}
+                    value={this.state.content}
+                    onChange={val => this.setState({content: val})} 
+                    onFocus={() => this.setState({isShowEmojis: false})}
+                    extra={<span>
+                        <span onClick={this.toggleShow}>😂   </span>
+                        <span onClick={this.handleSend}>send</span>
+                    </span>
+                    }></InputItem>
+                    {this.state.isShowEmojis? (<Grid data={this.emojis}
+                            columnNum={8}
+                            carouselMaxRow={4}
+                            isCarousel={true}
+                            onClick={(item) => {
+                                this.setState({content: this.state.content + item.text})
+                            }}
+                            itemStyle={{"touch-action": "pan-y"}}
+                            ></Grid>): null}
                 
                 </div>
             
